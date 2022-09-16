3d8ee803703bfccca64eaec348e0e5a967c22958
Tiago <tigolimab223@gmail.com>
2022-09-16 16:11:57 -0300

    map model Categories to categories

---
+++ prisma/migrations/20220916191134_added_map_to_categories_model/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Categories` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Categories";
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
