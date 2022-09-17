46055c9d7e300d08f4f2220b1f91b6899b94bc23
Tiago <tigolimab223@gmail.com>
2022-09-17 17:38:46 -0300

    /POST tests route and /GET tests route added

--- src/routers/testRouter.ts
+++ src/routers/testRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { getTest, postTest } from "../controllers/testController.js";
+import { ensureAuthenticatedMiddleware } from "../middlewares/authMiddleware.js";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
 import { testSchema } from "../schemas/testSchema.js";
 const testRouter = Router();
 
+testRouter.use(ensureAuthenticatedMiddleware);
 testRouter.post("/tests", validateSchemaMiddleware(testSchema), postTest);
 testRouter.get("/tests/:id", getTest);
 
